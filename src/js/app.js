875ce4323e736c5e6d58be4745100ed42fe06536
Mfonemmanuel <Mfonemmanuel@icloud.com>
2024-02-05 02:21:45 +0100

    update

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+// @copyright codewithemmanuel 2023
+
+'use strict';
+
+
+// Module import
+
+
+// Toggle sidebar in small screen
+
+//{HTMLElement}
+const $sidebar = document.querySelector('[data-sidebar]');
+// {Array<HTMLElement>} 
+const $sidebarTogglers = document.querySelector('[data-sidebar-toggler]');
+// {HTMLElement}
+const $overlay = document.querySelector('[data-sidebar-overlay')
